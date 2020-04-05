e32d15cfb3efadf419a8d0e0b419c9911f756517
Dinesh <34174856+dinesh2910@users.noreply.github.com>
2020-04-05 15:46:30 -0500

    InitialCommit

---
+++ AVLocalVideo/AVLocalVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVLocalVideo
+//
+//  Created by Dinesh Danda on 4/5/20.
+//  Copyright © 2020 Dinesh Danda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
